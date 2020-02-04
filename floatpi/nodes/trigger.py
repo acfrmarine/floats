a320e03ebaa673dc2f666414564ade3d0cabd5b6
auv <j.shields@acfr.usyd.edu.au>
2020-02-04 00:08:26 +0000

    Changed trigger to now use a time message

--- floatpi/nodes/trigger.py
+++ floatpi/nodes/trigger.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import time
 import rospy
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool,Time
 import RPi.GPIO as GPIO
 
 if __name__ == "__main__":
     rospy.init_node('camera_trigger_node')
-    trigger_pub = rospy.Publisher('trigger', Bool, queue_size=1)
+    trigger_pub = rospy.Publisher('trigger', Time, queue_size=1)
     trigger_pin = rospy.get_param('~trigger_pin', 26)
     trigger_rate = rospy.get_param('~trigger_rate', 2)  # in Hz. Defaults at 2Hz
     GPIO.setmode(GPIO.BCM)
@@ -23,9 +23,9 @@ if __name__ == "__main__":
         # Write low
         GPIO.output(trigger_pin, GPIO.LOW)
 
-        bmsg = Bool()
-        bmsg.data = True
-        trigger_pub.publish(bmsg)
+        tmsg = Time()
+        tmsg.data = rospy.get_rostime()
+        trigger_pub.publish(tmsg)
 
         r.sleep()
     GPIO.cleanup(trigger_pin)
