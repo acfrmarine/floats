6a7a9d13c27d85186d06d36cecde4311a1b769ba
Jackson Shields <jacksonhshields@gmail.com>
2020-02-17 21:05:33 -0800

    Added static thruster commands

--- etc/floatv1/pi/scripts/depth_control.sh
+++ etc/floatv1/pi/scripts/depth_control.sh
@@ -2,4 +2,4 @@
 source /opt/ros/kinetic/setup.bash
 source /home/pi/.bashrc
 source /home/pi/catkin_ws/devel/setup.bash
-roslaunch float_control dual_thruster_single_pid.launch
+roslaunch float_control depth_control_thruster_pid.launch
